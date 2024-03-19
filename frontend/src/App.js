cf95e6368aa5fb25a0b34c9628236bbe93a32e9b
aggelooskk <akermezo@yahoo.com>
2024-03-19 14:05:50 +0200

    New changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 const App = () => {
-  return <h1>Welcome To ProShop</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
