9266f1970c04bd09578750f16e22a038cff87d26
aggelooskk <akermezo@yahoo.com>
2024-04-03 19:03:21 +0300

    Header changes

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, Badge, Container } from "react-bootstrap";
+import { Navbar, Nav, Badge, Container, NavDropdown } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
 import { useSelector } from "react-redux";
@@ -7,7 +7,11 @@ import logo from "/Users/aggelosk/Desktop/Proshop/frontend/src/assets/logo.old.p
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
-  console.log(cartItems);
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const logoutHandler = () => {
+    console.log('logoutHandler');
+  }
 
   return (
     <header>
@@ -33,11 +37,22 @@ const Header = () => {
                   )}
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/login">
-                <Nav.Link>
-                  <FaUser /> Sign in
-                </Nav.Link>
-              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <FaUser /> Sign in
+                  </Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
