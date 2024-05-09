ba3b2678c28b79917f6330430441b0839e402f67
aggelooskk <akermezo@yahoo.com>
2024-05-09 17:10:25 +0300

    error fix

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/uploads");
+    cb(null, "uploads");
   },
   filename(req, file, cb) {
     cb(
@@ -20,7 +20,7 @@ function chechFileType(req, file, cb) {
   const filetypes = /jpg|jpeg|png/;
 
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const minetype = filetypes.test(file.mimetype);
+  const mimetype = filetypes.test(file.mimetype);
 
   if (extname && mimetype) {
     return cb(null, true);
